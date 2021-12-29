26072908287335d377b98c8d03aa439ac8e9463f
mabdulha <mozeebabdulha@gmail.com>
2021-12-29 22:13:02 +0000

    Edited home page dashboard to get the count values

--- src/components/HomeDashboard.vue
+++ src/components/HomeDashboard.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-    <h1>Dashboard Homepage</h1>
-    <p> {{ this.userCount }} </p>
-  </div>
+  <v-main>
+    <v-container>
+      <div>
+        <h1>Dashboard Homepage</h1>
+        <p> {{ this.userCount }} </p>
+        <p> {{ this.exerciseCount }} </p>
+        <p> {{ this.mealCount }} </p>
+      </div>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
+import ExerciseService from '../services/exerciseservice'
 import UserService from '../services/userservice'
+import MealService from '../services/mealservice'
 
 export default {
   data () {
     return {
-      userCount: null
+      userCount: null,
+      exerciseCount: null,
+      mealCount: null
     }
   },
   mounted () {
@@ -20,10 +30,26 @@ export default {
   methods: {
     getDashboardData () {
       this.getUserCount()
+      this.getExerciseCount()
+      this.getMealCount()
     },
     getUserCount () {
       UserService.fetchUserCount().then((result) => {
         this.userCount = result.data
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    getExerciseCount () {
+      ExerciseService.fetchExerciseCount().then((result) => {
+        this.exerciseCount = result.data
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    getMealCount () {
+      MealService.fetchMealCount().then((result) => {
+        this.mealCount = result.data
       }).catch((err) => {
         console.log(err)
       })
