017cf734eac001f724f2d6b5574142f1373019cc
mabdulha <mozeebabdulha@gmail.com>
2021-12-30 11:07:00 +0000

    Added meal info to the ccards

--- src/components/MealCard.vue
+++ src/components/MealCard.vue
@@ -1,72 +1,73 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md4 lg2 v-for="meal in meals" :key="meal.id">
+        <v-container fluid>
+          <v-card class="mx-auto" max-width="344">
+            <v-img
+              :src="meal.image"
+              height="200px"
+            ></v-img>
 
-    <v-card-title>
-      Top western road trips
-    </v-card-title>
+            <v-card-title> {{ meal.name }} </v-card-title>
 
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
+            <v-card-subtitle> Calories: {{ meal.calories }} </v-card-subtitle>
 
-    <v-card-actions>
-      <v-btn
-        color="orange lighten-2"
-        text
-      >
-        Explore
-      </v-btn>
+            <v-card-actions>
+              <v-btn color="cyan lighten-2" text> View </v-btn>
 
-      <v-spacer></v-spacer>
+              <v-spacer></v-spacer>
 
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
+              <v-btn icon @click="show = !show">
+                <v-icon>{{
+                  show ? "mdi-chevron-up" : "mdi-chevron-down"
+                }}</v-icon>
+              </v-btn>
+            </v-card-actions>
 
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
+            <v-expand-transition>
+              <div v-show="show">
+                <v-divider></v-divider>
 
-        <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-          <p> {{ meals }} </p>
-        </v-card-text>
-      </div>
-    </v-expand-transition>
-  </v-card>
+                <v-card-text>
+                  <h3>Nutrition Information</h3>
+                  <p>Energy: {{ meal.energy }}</p>
+                  <p>Carbs: {{ meal.carbs }}</p>
+                  <p>Sodium: {{ meal.sodium }}</p>
+                  <p>Fat: {{ meal.fat }}</p>
+                  <p>Protein: {{ meal.protein }}</p>
+                </v-card-text>
+              </div>
+            </v-expand-transition>
+          </v-card>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import MealService from '../services/mealservice'
+import MealService from "../services/mealservice";
 
 export default {
   data: () => ({
     show: false,
-    meals: []
+    meals: [],
   }),
-  created () {
-    this.loadMeals()
+  created() {
+    this.loadMeals();
   },
   methods: {
     loadMeals: function () {
-      MealService.fetchMeals().then((result) => {
-        this.meals = result.data
-        console.log(this.meals)
-      }).catch((err) => {
-        console.log(err)
-      });
-    }
-  }
-}
+      MealService.fetchMeals()
+        .then((result) => {
+          this.meals = result.data;
+          console.log(this.meals);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
