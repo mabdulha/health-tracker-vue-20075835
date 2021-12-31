0fc61471e2d6b915fc8e1600698436c3e46ce1a1
mabdulha <mozeebabdulha@gmail.com>
2021-12-31 13:30:06 +0000

    Cors error fix

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 export default() => {
   return axios.create({
-    baseURL: 'https://health-tracker-rest-20075835.herokuapp.com/api',
-    headers: {"Access-Control-Allow-Origin": "https://health-tracker-vue-20075835.herokuapp.com/"}
+    baseURL: 'https://health-tracker-vue-20075835.herokuapp.com/',
+    headers: {"Access-Control-Allow-Origin": "https://health-tracker-rest-20075835.herokuapp.com/api"}
   })
 }
