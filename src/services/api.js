e49b22cf1120de07832b9468861d89da421670ea
mabdulha <mozeebabdulha@gmail.com>
2021-12-31 13:23:18 +0000

    heroku deploy

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 
 export default() => {
   return axios.create({
-    baseURL: 'https://health-tracker-rest-20075835.herokuapp.com/api'
+    baseURL: 'https://health-tracker-rest-20075835.herokuapp.com/api',
+    headers: {"Access-Control-Allow-Origin": "*"}
   })
 }
