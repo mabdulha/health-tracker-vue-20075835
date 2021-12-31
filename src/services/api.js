26598ebc5743d7a110ae4f7304338cc49681db22
mabdulha <mozeebabdulha@gmail.com>
2021-12-31 12:02:03 +0000

    Firebase deploy

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 
 export default() => {
   return axios.create({
-    baseURL: 'https://cors-anywhere.herokuapp.com/https://health-tracker-rest-20075835.herokuapp.com/api'
+    baseURL: 'https://health-tracker-rest-20075835.herokuapp.com/api'
   })
 }
